bdd15199212153e3b617d6f29ddb72c1bfca620a
Ahmed.Dakrory <ahmedsonic1993@gmail.com>
2020-02-11 08:45:45 +0200

    move the search bar to the left

--- CarServiceCargoMarine/WebContent/resources/js/dashboard/assets/vendor/jquery-datatables-bs3/assets/js/datatables.js
+++ CarServiceCargoMarine/WebContent/resources/js/dashboard/assets/vendor/jquery-datatables-bs3/assets/js/datatables.js
@@ -8,7 +8,7 @@
 }(function ($) {
     /* Set the defaults for DataTables initialisation */
 	$.extend( true, $.fn.dataTable.defaults, {
-		"sDom": "<'row'<'col-sm-12'<'pull-right'f><'pull-left'l>r<'clearfix'>>>t<'row'<'col-sm-12'<'pull-left'i><'pull-right'p><'clearfix'>>>",
+		"sDom": "<'row'<'col-sm-12'<'pull-left'f><'pull-left'l>r<'clearfix'>>>t<'row'<'col-sm-12'<'pull-left'i><'pull-right'p><'clearfix'>>>",
 	    "sPaginationType": "bs_normal",
 	    "oLanguage": {
 	        "sLengthMenu": "Show _MENU_ Rows",
